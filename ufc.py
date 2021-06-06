b9eafa3ea8680af88867b8fd6cfb2cd02a6d1bc0
Douglas Richardson <dougiefresh1513@gmail.com>
2021-06-06 01:50:32 -0400

    System argument parameters allowed. Update pypputeer. Save of events in file for faster initial program start

--- ufc.py
+++ ufc.py
@@ -1,59 +1,75 @@
 # This is a sample Python script.
 import pyppeteer
 import asyncio
+import sys
+import json
 from pyppeteer import launch
 
 SHERDOG_UFC_EVENTS: str = 'https://www.sherdog.com/organizations/Ultimate-Fighting-Championship-UFC-2'
 SHERDOG: str = 'https://www.sherdog.com'
-PRODUCTION: bool = True
+PRODUCTION: bool = False
 SEARCH_TERMS = ['highlights', 'free fight']
 TIMEOUT_UNTIL_LOAD_FLAG = {'waitUntil': 'load', 'timeout': 0}
+HEADLINER_JS = '''(el) => {
+            let name = el.querySelector("h3 a")
+            return [name.getAttribute("href"), name.querySelector("span").innerText]
+        }'''
 
 # Possible RECORD standard for fighters = https://www.sherdog.com/fighter/$NAME
 
 
 class UFC:
-    __events = {}
+    __events = None
     _event = None
     __fights = {}
     _fight = None
 
     def __init__(self):
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.ufc_fight_comparison())
-        loop.close()
+
+        with open('cache.json') as cache_file:
+            cache_json = json.load(cache_file)
+            self.__events = cache_json
+
+        try:
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(self.ufc_fight_comparison())
+            loop.close()
+        finally:
+            with open('cache.json', 'w') as cache_file:
+                json.dump(self.__events, cache_file)
 
     async def ufc_fight_comparison(self):
+        # introduce process args
+        args = sys.argv
         browser = await pyppeteer.launch(headless=PRODUCTION, defaultViewport=None)
         page = await browser.newPage()
-        await page.goto(SHERDOG_UFC_EVENTS, TIMEOUT_UNTIL_LOAD_FLAG)
-        await page.waitForSelector("#upcoming_tab")
-        events_rows = await page.querySelectorAll("#upcoming_tab tr.odd, #upcoming_tab tr.even")
-        # print(events_rows)
-        for event_row in events_rows:
-            event_date = await event_row.querySelectorEval('td:nth-child(1)', '''(el) => {
-                return el.querySelector(".month").innerText + el.querySelector(".day").innerText;
-            }''')
-            event_td = await event_row.querySelectorEval('td:nth-child(2)', '''(el) => {
-                return [el.querySelector("a").getAttribute("href"), el.querySelector("span").innerText];
-            }''')
-            event_name = f'{event_date} {event_td[1]}'
-            self.__events[event_name] = event_td[0]
-
-        self._event = UFC.choosable_list(self.__events, "Which event are you looking for?  ")
-        print(f'{SHERDOG}{self._event}')
+        arg_len = len(args)
+        event = int(args[1]) if arg_len >= 2 else -1
+        if event == -1 or event not in self.__events:
+            await page.goto(SHERDOG_UFC_EVENTS, TIMEOUT_UNTIL_LOAD_FLAG)
+            await page.waitForSelector("#upcoming_tab")
+            events_rows = await page.querySelectorAll("#upcoming_tab tr.odd, #upcoming_tab tr.even")
+            # print(events_rows)
+            for event_row in events_rows:
+                event_date = await event_row.querySelectorEval('td:nth-child(1)', '''(el) => {
+                                    return el.querySelector(".month").innerText + el.querySelector(".day").innerText;
+                                }''')
+                event_td = await event_row.querySelectorEval('td:nth-child(2)', '''(el) => {
+                                    return [el.querySelector("a").getAttribute("href"), el.querySelector("span").innerText];
+                                }''')
+                event_name = f'{event_date} {event_td[1]}'
+                self.__events[event_name] = event_td[0]
+            self._event = UFC.choosable_list(self.__events, "Which event are you looking for?  ")
+        else:
+            self._event = self.__events[event]
+            print(f'{SHERDOG}{self._event}')
+        print(self._event)
         await page.goto(f'{SHERDOG}{self._event}', TIMEOUT_UNTIL_LOAD_FLAG)
-        await asyncio.sleep(5)
-        await page.waitForSelector(".col_left")
-        # TODO: Main Event
-        headliner_js = '''(el) => {
-            let name = el.querySelector("h3 a")
-            return [name.getAttribute("href"), name.querySelector("span").innerText]
-        }'''
-        headliner_one = await page.querySelectorEval('div.fighter.left_side', headliner_js)
-        headliner_two = await page.querySelectorEval('div.fighter.right_side', headliner_js)
-        self.__fights[UFC.fighter_name(headliner_one, headliner_two)] = (headliner_one, headliner_two)
 
+        await page.waitForSelector(".col_left")
+        headliner_one = await page.querySelectorEval('div.fighter.left_side', HEADLINER_JS)
+        headliner_two = await page.querySelectorEval('div.fighter.right_side', HEADLINER_JS)
+        self.__fights[UFC.fighter_name(headliner_one, headliner_two)] = (headliner_one, headliner_two)
         card_fights = await page.querySelectorAll('.module.event_match tr.odd, tr.even')
         for fight in card_fights:
             fighter_one = await fight.querySelectorEval('td.text_right a', '''(el) => {
@@ -64,13 +80,18 @@ class UFC:
             }''')
             fight_name = UFC.fighter_name(fighter_one, fighter_two)
             self.__fights[fight_name] = (fighter_one, fighter_two)
-        self._fight = self.choosable_list(self.__fights, "Which fight would you like to research? ")
+
+        if arg_len >= 3 and str(args[3]) in self.__fights:
+            self._fight = self.__fights[str(args[3])]
+        else:
+            self._fight = self.choosable_list(self.__fights, "Which fight would you like to research? ")
+        await browser.close()
         for fighter in self._fight:
             print(fighter)
             await UFC.open_fighter_browser(fighter)
+        await asyncio.sleep(5)
 
-        await asyncio.sleep(60*20)
-        await browser.close()
+        print('Shutting Down Correctly')
 
     @staticmethod
     def fighter_name(fighter_one, fighter_two):
